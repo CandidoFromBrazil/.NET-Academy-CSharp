eb9c735528097a0144fc44d1c46dd4ea6c35cfa4
Matheus Willians Fernandes Candido <102835123+CandidoFromBrazil@users.noreply.github.com>
2022-10-12 17:38:26 -0300

    Add files via upload

---
+++ Take your first steps with C#/Csharp03.cs
@@ -0,0 +1,22 @@
+using System;
+namespace dotNetAcademy
+{
+    public class Csharp03
+    {  public static void Main(string [] args)
+        {
+            //declaring variables
+            char userOption;
+
+            int gameScore;
+
+            decimal particlesPerMillion;
+        
+            float moneyBorrowed;
+
+            double moneyLended;
+
+            bool processedCustomer;
+
+        }
+    }
+}
