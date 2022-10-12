bcbf144d0b2efef9c8433f9151496788ee78c310
Matheus Willians Fernandes Candido <102835123+CandidoFromBrazil@users.noreply.github.com>
2022-10-12 17:18:00 -0300

    Add files via upload

---
+++ Take your first steps with C#/Csharp01.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace dotNetAcademy
+{
+    public class Csharp01
+    {  static void Main(string [] args)
+        {
+            Console.WriteLine("This is my first line");
+            Console.WriteLine("This is my second line");
+        }
+    }
+}
---
+++ Take your first steps with C#/Csharp02.cs
@@ -0,0 +1,25 @@
+using System;
+namespace dotNetAcademy
+{
+    public class Csharp02
+    {  public static void Main(string [] args)
+        {
+            //literal data types + Console.WriteLine();
+            //char
+            Console.WriteLine('x');
+            //string
+            Console.WriteLine("Mattews");
+            //int
+            Console.WriteLine(452);
+            //decimal
+            Console.WriteLine(60.0m);
+            //float
+            Console.WriteLine(10.0f);
+            //double
+            Console.WriteLine(10.00000000);
+            //bool
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+        }
+    }
+}
