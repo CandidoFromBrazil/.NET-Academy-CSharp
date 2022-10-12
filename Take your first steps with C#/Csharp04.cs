ff2050c0e759efb2cfeb49b23ecb24582c39f889
Matheus Willians Fernandes Candido <102835123+CandidoFromBrazil@users.noreply.github.com>
2022-10-12 18:39:01 -0300

    Add files via upload

---
+++ Take your first steps with C#/Csharp04.cs
@@ -0,0 +1,13 @@
+using System;
+namespace dotNetAcademy
+{
+    public class Csharp04
+    {  public static void Main(string [] args)
+        {
+            //assign valiues to variables
+            string firstName;
+            firstName = "Matthew";
+            Console.WriteLine(firstName.ToUpper());
+        }
+    }
+}
